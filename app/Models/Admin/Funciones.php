4d44d1b898920ff47e6cbc2ac56c94280c7da0a7
Carlos Gustavo Trejo Bárcenas <76748814+CarlosGs22@users.noreply.github.com>
2022-01-04 15:22:00 -0600

    cleanInput
    cleanInput

--- app/Models/Admin/Funciones.php
+++ app/Models/Admin/Funciones.php
@@ -74,4 +74,43 @@ class Funciones
             return $data;
         }
     }
+
+    function cleanInput($input) {
+ 
+        $search = array(
+          '@<script[^>]*?>.*?</script>@si',   // Elimina javascript
+          '@<[\/\!]*?[^<>]*?>@si',            // Elimina las etiquetas HTML
+          '@<style[^>]*?>.*?</style>@siU',    // Elimina las etiquetas de estilo
+          '@<![\s\S]*?--[ \t\n\r]*>@'         // Elimina los comentarios multi-línea
+        );
+       
+          $output = preg_replace($search, '', $input);
+          return $output;
+        }
+       
+      function sanitize($input) {
+          if (is_array($input)) {
+              foreach($input as $var=>$val) {
+                  $output[$var] = sanitize($val);
+              }
+          }
+          else {
+              if (get_magic_quotes_gpc()) {
+                  $input = stripslashes($input);
+              }
+              $input  = cleanInput($input);
+              $output = mysql_real_escape_string($input);
+          }
+          return $output;
+      }
+
+      public function cleanSanitize($value)
+      {
+        $step1 = sanitize($value);
+        $step2 = strip_tags($step1); // Strip tags
+        $step3 = htmlspecialchars($step2); // Change tag characters to HTML entities
+        return $step3;
+      }
+
+
 }
