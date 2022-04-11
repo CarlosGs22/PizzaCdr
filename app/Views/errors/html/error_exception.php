c408848d30ae7bf27b17bdd33be0da2f1b873824
CarlosGs22 <carlosgs.trejo@gmail.com>
2022-04-10 22:53:00 -0500

    issue9
    Se agrega pagina 500 error

--- app/Views/errors/html/error_exception.php
+++ app/Views/errors/html/error_exception.php
@@ -1,401 +1,260 @@
-<?php $error_id = uniqid('error', true); ?>
-<!doctype html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<meta name="robots" content="noindex">
-
-	<title><?= htmlspecialchars($title, ENT_SUBSTITUTE, 'UTF-8') ?></title>
-	<style type="text/css">
-		<?= preg_replace('#[\r\n\t ]+#', ' ', file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'debug.css')) ?>
-	</style>
-
-	<script type="text/javascript">
-		<?= file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'debug.js') ?>
-	</script>
-</head>
-<body onload="init()">
-
-	<!-- Header -->
-	<div class="header">
-		<div class="container">
-			<h1><?= htmlspecialchars($title, ENT_SUBSTITUTE, 'UTF-8'), ($exception->getCode() ? ' #' . $exception->getCode() : '') ?></h1>
-			<p>
-				<?= $exception->getMessage() ?>
-				<a href="https://www.google.com/search?q=<?= urlencode($title . ' ' . preg_replace('#\'.*\'|".*"#Us', '', $exception->getMessage())) ?>"
-				   rel="noreferrer" target="_blank">search &rarr;</a>
-			</p>
-		</div>
-	</div>
-
-	<!-- Source -->
-	<div class="container">
-		<p><b><?= static::cleanPath($file, $line) ?></b> at line <b><?= $line ?></b></p>
-
-		<?php if (is_file($file)) : ?>
-			<div class="source">
-				<?= static::highlightFile($file, $line, 15); ?>
-			</div>
-		<?php endif; ?>
-	</div>
-
-	<div class="container">
-
-		<ul class="tabs" id="tabs">
-			<li><a href="#backtrace">Backtrace</a></li>
-				<li><a href="#server">Server</a></li>
-				<li><a href="#request">Request</a></li>
-				<li><a href="#response">Response</a></li>
-				<li><a href="#files">Files</a></li>
-				<li><a href="#memory">Memory</a></li>
-			</li>
-		</ul>
-
-		<div class="tab-content">
-
-			<!-- Backtrace -->
-			<div class="content" id="backtrace">
-
-				<ol class="trace">
-				<?php foreach ($trace as $index => $row) : ?>
-
-					<li>
-						<p>
-							<!-- Trace info -->
-							<?php if (isset($row['file']) && is_file($row['file'])) :?>
-								<?php
-								if (isset($row['function']) && in_array($row['function'], ['include', 'include_once', 'require', 'require_once']))
-									{
-									echo $row['function'] . ' ' . static::cleanPath($row['file']);
-								}
-								else
-									{
-									echo static::cleanPath($row['file']) . ' : ' . $row['line'];
-								}
-								?>
-							<?php else : ?>
-								{PHP internal code}
-							<?php endif; ?>
-
-							<!-- Class/Method -->
-							<?php if (isset($row['class'])) : ?>
-								&nbsp;&nbsp;&mdash;&nbsp;&nbsp;<?= $row['class'] . $row['type'] . $row['function'] ?>
-								<?php if (! empty($row['args'])) : ?>
-									<?php $args_id = $error_id . 'args' . $index ?>
-									( <a href="#" onclick="return toggle('<?= $args_id ?>');">arguments</a> )
-									<div class="args" id="<?= $args_id ?>">
-										<table cellspacing="0">
-
-										<?php
-										$params = null;
-										// Reflection by name is not available for closure function
-										if (substr( $row['function'], -1 ) !== '}')
-										{
-											$mirror = isset( $row['class'] ) ? new \ReflectionMethod( $row['class'], $row['function'] ) : new \ReflectionFunction( $row['function'] );
-											$params = $mirror->getParameters();
-										}
-										foreach ($row['args'] as $key => $value) : ?>
-											<tr>
-												<td><code><?= htmlspecialchars(isset($params[$key]) ? '$' . $params[$key]->name : "#$key", ENT_SUBSTITUTE, 'UTF-8') ?></code></td>
-												<td><pre><?= print_r($value, true) ?></pre></td>
-											</tr>
-										<?php endforeach ?>
-
-										</table>
-									</div>
-								<?php else : ?>
-									()
-								<?php endif; ?>
-							<?php endif; ?>
-
-							<?php if (! isset($row['class']) && isset($row['function'])) : ?>
-								&nbsp;&nbsp;&mdash;&nbsp;&nbsp;	<?= $row['function'] ?>()
-							<?php endif; ?>
-						</p>
-
-						<!-- Source? -->
-						<?php if (isset($row['file']) && is_file($row['file']) &&  isset($row['class'])) : ?>
-							<div class="source">
-								<?= static::highlightFile($row['file'], $row['line']) ?>
-							</div>
-						<?php endif; ?>
-					</li>
-
-				<?php endforeach; ?>
-				</ol>
-
-			</div>
-
-			<!-- Server -->
-			<div class="content" id="server">
-				<?php foreach (['_SERVER', '_SESSION'] as $var) : ?>
-					<?php if (empty($GLOBALS[$var]) || ! is_array($GLOBALS[$var]))
-					{
-						continue;
-					} ?>
-
-					<h3>$<?= $var ?></h3>
-
-					<table>
-						<thead>
-							<tr>
-								<th>Key</th>
-								<th>Value</th>
-							</tr>
-						</thead>
-						<tbody>
-						<?php foreach ($GLOBALS[$var] as $key => $value) : ?>
-							<tr>
-								<td><?= htmlspecialchars($key, ENT_IGNORE, 'UTF-8') ?></td>
-								<td>
-									<?php if (is_string($value)) : ?>
-										<?= htmlspecialchars($value, ENT_SUBSTITUTE, 'UTF-8') ?>
-									<?php else: ?>
-										<?= '<pre>' . print_r($value, true) ?>
-									<?php endif; ?>
-								</td>
-							</tr>
-						<?php endforeach; ?>
-						</tbody>
-					</table>
-
-				<?php endforeach ?>
-
-				<!-- Constants -->
-				<?php $constants = get_defined_constants(true); ?>
-				<?php if (! empty($constants['user'])) : ?>
-					<h3>Constants</h3>
-
-					<table>
-						<thead>
-							<tr>
-								<th>Key</th>
-								<th>Value</th>
-							</tr>
-						</thead>
-						<tbody>
-						<?php foreach ($constants['user'] as $key => $value) : ?>
-							<tr>
-								<td><?= htmlspecialchars($key, ENT_IGNORE, 'UTF-8') ?></td>
-								<td>
-									<?php if (! is_array($value) && ! is_object($value)) : ?>
-										<?= htmlspecialchars($value, ENT_SUBSTITUTE, 'UTF-8') ?>
-									<?php else: ?>
-										<?= '<pre>' . print_r($value, true) ?>
-									<?php endif; ?>
-								</td>
-							</tr>
-						<?php endforeach; ?>
-						</tbody>
-					</table>
-				<?php endif; ?>
-			</div>
-
-			<!-- Request -->
-			<div class="content" id="request">
-				<?php $request = \Config\Services::request(); ?>
-
-				<table>
-					<tbody>
-						<tr>
-							<td style="width: 10em">Path</td>
-							<td><?= $request->uri ?></td>
-						</tr>
-						<tr>
-							<td>HTTP Method</td>
-							<td><?= $request->getMethod(true) ?></td>
-						</tr>
-						<tr>
-							<td>IP Address</td>
-							<td><?= $request->getIPAddress() ?></td>
-						</tr>
-						<tr>
-							<td style="width: 10em">Is AJAX Request?</td>
-							<td><?= $request->isAJAX() ? 'yes' : 'no' ?></td>
-						</tr>
-						<tr>
-							<td>Is CLI Request?</td>
-							<td><?= $request->isCLI() ? 'yes' : 'no' ?></td>
-						</tr>
-						<tr>
-							<td>Is Secure Request?</td>
-							<td><?= $request->isSecure() ? 'yes' : 'no' ?></td>
-						</tr>
-						<tr>
-							<td>User Agent</td>
-							<td><?= $request->getUserAgent()->getAgentString() ?></td>
-						</tr>
-
-					</tbody>
-				</table>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 
-				<?php $empty = true; ?>
-				<?php foreach (['_GET', '_POST', '_COOKIE'] as $var) : ?>
-					<?php if (empty($GLOBALS[$var]) || ! is_array($GLOBALS[$var]))
-					{
-						continue;
-					} ?>
+<style>
+h4{
+        color: #253858;
+        margin-bottom: .8rem;
+        position: relative;
+        font-family: 'Raleway', sans-serif;
+        font-size: 1.5rem;
+    }
+    p{
+        margin-top: 0;
+        margin-bottom: 1rem;
+        display: block;
+        margin-block-start: 1em;
+        margin-block-end: 1em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        color: rgb(113, 120, 126);
+        font-family: 'Lato', sans-serif;
+    }
+    .btn-primary:hover {
+        color: #fff;
+        background-color: #0069d9;
+        border-color: #0062cc;
+    }
+    .btn-primary {
+        color: #fff;
+        background-color: #0069d9;
+        border-color: #0062cc;
+    }
+    .btn-round {
+        border-radius: 30px !important;
+        text-decoration: none;
+    }
 
-					<?php $empty = false; ?>
+    .btn {
+        font-size: 15px;
+        font-weight: 600;
+        padding: 9px 25px;
+        border-width: 2px;
+        box-shadow: 0 3px 8px 0 rgba(41,49,89,.15), inset 0 0 0 1px hsla(0,0%,100%,.1);
+    }
+    .btn {
+        display: inline-block;
+        font-weight: 400;
+        text-align: center;
+        white-space: nowrap;
+        vertical-align: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        border: 1px solid transparent;
+        padding: .375rem .75rem;
+        font-size: 1rem;
+        line-height: 1.5;
+        border-radius: .25rem;
+        transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    }
+    .btn:not(:disabled):not(.disabled) {
+        cursor: pointer;
+    }
+    /*[404 error page]
+--------------------------*/
+    .error-content {
+        padding: 0 0 70px;
+    }
+    .error-text{
+        text-align: center;
+    }
+    .error {
+        font-size: 180px;
+        font-weight: 100;
+    }
+    @keyframes bob {
+        0% {
+            top: 0;
+        }
+        50% {
+            top: 0.2em;
+        }
+    }
+    .im-sheep {
+        display: inline-block;
+        position: relative;
+        font-size: 1em;
+        margin-bottom: 70px;
+    }
+    .im-sheep * {
+        transition: transform 0.3s;
+    }
+    .im-sheep .top {
+        position: relative;
+        top: 0;
+        animation: bob 1s infinite;
+    }
+    .im-sheep:hover .head {
+        transform: rotate(0deg);
+    }
+    .im-sheep:hover .head .im-eye {
+        width: 1.25em;
+        height: 1.25em;
+    }
+    .im-sheep:hover .head .im-eye:before {
+        right: 30%;
+    }
+    .im-sheep:hover .top {
+        animation-play-state: paused;
+    }
+    .im-sheep .head {
+        display: inline-block;
+        width: 5em;
+        height: 5em;
+        border-radius: 100%;
+        background: #253858;
+        vertical-align: middle;
+        position: relative;
+        top: 1em;
+        transform: rotate(30deg);
+    }
+    .im-sheep .head:before {
+        content: '';
+        display: inline-block;
+        width: 80%;
+        height: 50%;
+        background: #253858;
+        position: absolute;
+        bottom: 0;
+        right: -10%;
+        border-radius: 50% 40%;
+    }
+    .im-sheep .head:hover .im-ear.one, .im-sheep .head:hover .im-ear.two {
+        transform: rotate(0deg);
+    }
+    .im-sheep .head .im-eye {
+        display: inline-block;
+        width: 1em;
+        height: 1em;
+        border-radius: 100%;
+        background: white;
+        position: absolute;
+        overflow: hidden;
+    }
+    .im-sheep .head .im-eye:before {
+        content: '';
+        display: inline-block;
+        background: black;
+        width: 50%;
+        height: 50%;
+        border-radius: 100%;
+        position: absolute;
+        right: 10%;
+        bottom: 10%;
+        transition: all 0.3s;
+    }
+    .im-sheep .head .im-eye.one {
+        right: -2%;
+        top: 1.7em;
+    }
+    .im-sheep .head .im-eye.two {
+        right: 2.5em;
+        top: 1.7em;
+    }
+    .im-sheep .head .im-ear {
+        background: #253858;
+        width: 50%;
+        height: 30%;
+        border-radius: 100%;
+        position: absolute;
+    }
+    .im-sheep .head .im-ear.one {
+        left: -10%;
+        top: 5%;
+        transform: rotate(-30deg);
+    }
+    .im-sheep .head .im-ear.two {
+        top: 2%;
+        right: -5%;
+        transform: rotate(20deg);
+    }
+    .im-sheep .body {
+        display: inline-block;
+        width: 7em;
+        height: 7em;
+        border-radius: 100%;
+        background: #0054D1;
+        position: relative;
+        vertical-align: middle;
+        margin-right: -3em;
+    }
+    .im-sheep .im-legs {
+        display: inline-block;
+        position: absolute;
+        top: 80%;
+        left: 10%;
+        z-index: -1;
+    }
+    .im-sheep .im-legs .im-leg {
+        display: inline-block;
+        background: #141214;
+        width: 0.5em;
+        height: 2.5em;
+        margin: 0.2em;
+    }
+    .im-sheep::before {
+        left: 0;
+        content: '';
+        display: inline-block;
+        position: absolute;
+        top: 112%;
+        width: 100%;
+        height: 18%;
+        border-radius: 100%;
+        background: rgba(0, 0, 0, 0.2);
+    }
 
-					<h3>$<?= $var ?></h3>
 
-					<table style="width: 100%">
-						<thead>
-							<tr>
-								<th>Key</th>
-								<th>Value</th>
-							</tr>
-						</thead>
-						<tbody>
-						<?php foreach ($GLOBALS[$var] as $key => $value) : ?>
-							<tr>
-								<td><?= htmlspecialchars($key, ENT_IGNORE, 'UTF-8') ?></td>
-								<td>
-									<?php if (! is_array($value) && ! is_object($value)) : ?>
-										<?= htmlspecialchars($value, ENT_SUBSTITUTE, 'UTF-8') ?>
-									<?php else: ?>
-										<?= '<pre>' . print_r($value, true) ?>
-									<?php endif; ?>
-								</td>
-							</tr>
-						<?php endforeach; ?>
-						</tbody>
-					</table>
 
-				<?php endforeach ?>
 
-				<?php if ($empty) : ?>
 
-					<div class="alert">
-						No $_GET, $_POST, or $_COOKIE Information to show.
-					</div>
 
-				<?php endif; ?>
 
-				<?php $headers = $request->getHeaders(); ?>
-				<?php if (! empty($headers)) : ?>
+</style>
 
-					<h3>Headers</h3>
 
-					<table>
-						<thead>
-							<tr>
-								<th>Header</th>
-								<th>Value</th>
-							</tr>
-						</thead>
-						<tbody>
-						<?php foreach ($headers as $value) : ?>
-							<?php if (empty($value))
-							{
-								continue;
-							} ?>
-							<?php if (! is_array($value))
-							{
-								$value = [$value];
-							} ?>
-							<?php foreach ($value as $h) : ?>
-								<tr>
-									<td><?= esc($h->getName(), 'html') ?></td>
-									<td><?= esc($h->getValueLine(), 'html') ?></td>
-								</tr>
-							<?php endforeach; ?>
-						<?php endforeach; ?>
-						</tbody>
-					</table>
-
-				<?php endif; ?>
-			</div>
-
-			<!-- Response -->
-			<?php
-				$response = \Config\Services::response();
-				$response->setStatusCode(http_response_code());
-			?>
-			<div class="content" id="response">
-				<table>
-					<tr>
-						<td style="width: 15em">Response Status</td>
-						<td><?= $response->getStatusCode() . ' - ' . $response->getReason() ?></td>
-					</tr>
-				</table>
-
-				<?php $headers = $response->getHeaders(); ?>
-				<?php if (! empty($headers)) : ?>
-					<?php natsort($headers) ?>
-
-					<h3>Headers</h3>
-
-					<table>
-						<thead>
-							<tr>
-								<th>Header</th>
-								<th>Value</th>
-							</tr>
-						</thead>
-						<tbody>
-						<?php foreach ($headers as $name => $value) : ?>
-							<tr>
-								<td><?= esc($name, 'html') ?></td>
-								<td><?= esc($response->getHeaderLine($name), 'html') ?></td>
-							</tr>
-						<?php endforeach; ?>
-						</tbody>
-					</table>
-
-				<?php endif; ?>
-			</div>
-
-			<!-- Files -->
-			<div class="content" id="files">
-				<?php $files = get_included_files(); ?>
-
-				<ol>
-				<?php foreach ($files as $file) :?>
-					<li><?= htmlspecialchars( static::cleanPath($file), ENT_SUBSTITUTE, 'UTF-8') ?></li>
-				<?php endforeach ?>
-				</ol>
-			</div>
-
-			<!-- Memory -->
-			<div class="content" id="memory">
-
-				<table>
-					<tbody>
-						<tr>
-							<td>Memory Usage</td>
-							<td><?= static::describeMemory(memory_get_usage(true)) ?></td>
-						</tr>
-						<tr>
-							<td style="width: 12em">Peak Memory Usage:</td>
-							<td><?= static::describeMemory(memory_get_peak_usage(true)) ?></td>
-						</tr>
-						<tr>
-							<td>Memory Limit:</td>
-							<td><?= ini_get('memory_limit') ?></td>
-						</tr>
-					</tbody>
-				</table>
-
-			</div>
-
-		</div>  <!-- /tab-content -->
-
-	</div> <!-- /container -->
-
-	<div class="footer">
-		<div class="container">
-
-			<p>
-				Displayed at <?= date('H:i:sa') ?> &mdash;
-				PHP: <?= phpversion() ?>  &mdash;
-				CodeIgniter: <?= \CodeIgniter\CodeIgniter::CI_VERSION ?>
-			</p>
-
-		</div>
-	</div>
-
-</body>
-</html>
+<div class="error-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 ">
+                <div class="error-text">
+                    <h1 class="error">500 Error</h1>
+                    <div class="im-sheep">
+                        <div class="top">
+                            <div class="body"></div>
+                            <div class="head">
+                                <div class="im-eye one"></div>
+                                <div class="im-eye two"></div>
+                                <div class="im-ear one"></div>
+                                <div class="im-ear two"></div>
+                            </div>
+                        </div>
+                        <div class="im-legs">
+                            <div class="im-leg"></div>
+                            <div class="im-leg"></div>
+                            <div class="im-leg"></div>
+                            <div class="im-leg"></div>
+                        </div>
+                    </div>
+                    <h4>Oops! Ocurrió un error interno</h4>
+                    <p>Disculpa las demoras estamos resolviéndolo, en breve regresamos!</p>
+                    <a href="<?=base_url()?>" class="btn btn-primary btn-round">Ir a la página de inicio</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
